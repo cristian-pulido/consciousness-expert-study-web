74d29fe09b2f143fc383820f05d4a4cebcf4cb5f
Cristian Pulido <cristian-pulido@users.noreply.github.com>
2026-09-22 20:09:58 -0400

    Point public shell to current study deployment

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.STUDY_CONFIG = {
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbw5dUSA2pxR3aw2a5IMjWLq8q2S3kIqqRX98i6_Yo9VB__10xGkK42j1r8MxH29imVSBg/exec'
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzhyfYu4Tnv4uQKWbmogt2K47StYy_HaVLJ33cdhKt9avI3v3jIfGc9wzJ4nSzpEhJKEA/exec'
 };
